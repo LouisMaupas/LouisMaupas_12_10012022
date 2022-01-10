201e2eb02586b39e8dcda1b42b3b42a186041aae
lmaupas <louis.maupas@framatome.com>
2022-01-10 16:40:25 +0100

    react router initialized

--- sportsee/src/index.jsx
+++ sportsee/src/index.jsx
@@ -1,11 +1,29 @@
 import { render } from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
 
 const rootElement = document.getElementById("root");
 render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="expenses" element={<Expenses />} />
+      <Route path="invoices" element={<Invoices />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>404</p>
+          </main>
+        }
+      />
+    </Routes>
   </BrowserRouter>,
   rootElement
 );
