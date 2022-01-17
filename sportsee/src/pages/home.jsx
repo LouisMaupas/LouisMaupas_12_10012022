6d11177cb77218a23760feae4a442ae62494ce67
lmaupas <louis.maupas@framatome.com>
2022-01-17 19:26:21 +0100

    WIP call API with useEffect & fetch

--- sportsee/src/pages/home.jsx
+++ sportsee/src/pages/home.jsx
@@ -1,10 +1,33 @@
+// React
+import React, { useState, useEffect } from 'react';
+
+// App
 import Welcome from "../components/Welcome";
+import data from "../backend/data_mocked";
 
 
-export default function Home() {
+
+// Component
+function Home() {
+
+    // Call API
+    useEffect(() => {
+        fetch(data)
+            .then((response) => response.json()
+            .then(({ myData }) => console.log(myData))
+            .catch((error) => console.log(error))
+        )
+    }, [])
+
+    
+
   return (
     <div>
         <Welcome></Welcome>
     </div>
     )
 }
+
+
+
+export default Home;
