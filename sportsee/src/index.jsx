d8784ee9d631b81f7dd75d6bb00eb080b26d6a7b
lmaupas <louis.maupas@framatome.com>
2022-01-17 18:16:58 +0100

    WIP

--- sportsee/src/index.jsx
+++ sportsee/src/index.jsx
@@ -1,25 +1,32 @@
+// React
 import { render } from "react-dom";
 import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
+
+// App
 import App from "./App";
+import {UserData} from "./utils/context/index";
 
 const rootElement = document.getElementById("root");
 render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route
-        path="*"
-        element={
-          <main>
-            <p>404</p>
-          </main>
-        }
-      />
-    </Routes>
-  </BrowserRouter>,
+  // <UserData>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App/>} />
+        <Route path='/user/:id' element={<App/>}></Route>
+        <Route
+          path="*"
+          element={
+            <main>
+              <p>404</p>
+            </main>
+          }
+        />
+      </Routes>
+    </BrowserRouter>,
+  // </UserData>,
   rootElement
 );
