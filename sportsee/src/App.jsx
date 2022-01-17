f2f5e19d7bd3e67703a5a9fe3d21b13fdfa3ecbb
lmaupas <louis.maupas@framatome.com>
2022-01-17 14:34:01 +0100

    WIP side-menu

--- sportsee/src/App.jsx
+++ sportsee/src/App.jsx
@@ -1,14 +1,16 @@
 import Header from "./components/Header"
 import SideMenu from "./components/SideMenu"
+import index from "./index.css"
+
 
 
 export default function App() {
   return (
     <div>
       <Header></Header>
-      <div>
+      <div className="d-flex">
         <SideMenu></SideMenu>
-        <div></div>
+        <div>MAIN DISPLAY</div>
       </div>
     </div>
   );
