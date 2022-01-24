aa3826b7de7146f74f0e15bb4e1a8f8c05077d04
lmaupas <louis.maupas@framatome.com>
2022-01-24 13:08:42 +0100

    :build: axios

--- sportsee/src/pages/home.jsx
+++ sportsee/src/pages/home.jsx
@@ -5,21 +5,34 @@ import React, { useState, useEffect } from 'react';
 import Welcome from "../components/Welcome";
 import data from "../backend/data_mocked";
 
-
-
-
 // Component
 function Home() {
 
     /**
      * Call API with axios => https://github.com/axios/axios
+     * Make a request for a user with a given ID
      */
     useEffect(() => {
-        fetch(data)
-            .then((response) => response.json()
-            .then(({ myData }) => console.log(myData))
-            .catch((error) => console.log(error))
-        )
+
+        const axios = require('axios');
+        const apiURL = data
+        let userId = 18
+
+
+        axios.get(`${apiURL}`, {
+            params: {
+              ID: userId
+            }
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          })
+          .then(function () {
+            // always executed
+          }); 
     }, [])
 
     
