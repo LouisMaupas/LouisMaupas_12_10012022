d342a7e28469bbea5d9e1292706715ef3f4418a8
lmaupas <louis.maupas@framatome.com>
2022-01-24 15:34:55 +0100

    :feat: (chart) WIP chart component

---
+++ sportsee/src/components/Chart/index.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+// styled-component
+const ChartLabel = styled.div`
+  background: red;
+  width: 258px;
+  Height: 263px;
+  border-radius: 5px;
+`
+
+function Chart() {
+    return (
+        <ChartLabel>
+            
+        </ChartLabel>
+    )
+  }
