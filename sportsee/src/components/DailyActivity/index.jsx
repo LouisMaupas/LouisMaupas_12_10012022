347a4856475c2c4bbdb0c5930f5702fef7e2e77f
lmaupas <louis.maupas@framatome.com>
2022-01-24 14:59:41 +0100

    :feat: (DailyActivity) WIP daily activities chart

---
+++ sportsee/src/components/DailyActivity/index.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
+
+export default function DailyActivity({profils}) {
+
+    // styled-component
+    const DailyActivityLabel = styled.div`
+    background-color: yellow;
+    `
+
+    //Recharts
+    const data = [{name: 'Page A', uv: 400, pv: 2400, amt: 2400}]
+    const renderLineChart = (
+        <LineChart width={600} height={300} data={data}>
+          <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+          <CartesianGrid stroke="#ccc" />
+          <XAxis dataKey="name" />
+          <YAxis />
+        </LineChart>
+      );
+
+    return (
+      <DailyActivityLabel>
+          <div>
+            {renderLineChart}
+          </div>
+      </DailyActivityLabel>
+    )
+}
