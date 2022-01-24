8ac8600773d5209fb90da96937e48543cfba4457
lmaupas <louis.maupas@framatome.com>
2022-01-24 14:05:33 +0100

    :style: (homepage)

--- sportsee/src/App.jsx
+++ sportsee/src/App.jsx
@@ -2,17 +2,25 @@ import Header from "./components/Header"
 import Home from "./pages/home/home"
 import SideMenu from "./components/SideMenu"
 import index from "./index.css"
+import styled from "styled-components"
 
+
+// styled-component
+const ContainerLabel = styled.div`
+  background: rgb(240,248,255);
+  min-width: 1024px;
+  min-height: 700px
+`
 
 
 export default function App() {
   return (
-    <div>
+    <ContainerLabel>
       <Header></Header>
       <div className="d-flex">
         <SideMenu></SideMenu>
         <Home></Home>
       </div>
-    </div>
+    </ContainerLabel>
   );
 }
