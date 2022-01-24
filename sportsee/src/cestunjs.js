450401c240d29f0640f54a1b7af1d5792ce1f3e8
lmaupas <louis.maupas@framatome.com>
2022-01-24 12:58:13 +0100

    :misc: WIP basic strucutre

---
+++ sportsee/src/cestunjs.js
@@ -0,0 +1,22 @@
+
+/**
+ * Multiplie 2 nombres !
+ * @param {number} a le premicher chiffre
+ * @param {number} b le 2nd chiffre
+ * @returns 
+ */
+function multiplier(a,b) {
+    return a*b
+}
+
+
+/**
+ * Premier admin
+ * @type {{id : number, isAdmin; boolean, pseudo: string|number, ville : string}}
+ */
+const user1 = {
+    id: 1,
+    isAdmin: true,
+    pseudo: "c'est mon psuedo",
+    ville: "Lyon"
+}
