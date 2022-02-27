8ea30a526d2c2d2c66c85935d0940253c938ad57
lmaupas <louis.maupas@framatome.com>
2022-02-27 21:49:29 +0100

    ui

--- sportsee/src/components/Cards/index.jsx
+++ sportsee/src/components/Cards/index.jsx
@@ -1,44 +1,46 @@
 import styled from "styled-components";
-import PropTypes from 'prop-types';
-
+import PropTypes from "prop-types";
 
 // styled-component
 const CardLabel = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background: #FBFBFB;
+  background: #fbfbfb;
   width: 258px;
-  Height: 124px;
+  height: 124px;
   border-radius: 5px;
+  margin-bottom: 39px;
   > div .card__value {
     font-weight: bold;
-    font-size: 20px
+    font-size: 20px;
   }
   > div .card__type {
-    font-size: 14px
+    font-size: 14px;
   }
-`
+`;
 
 // Component
 /**
  * Generate cards
- * @param {*} param0 
+ * @param {*} param0
  * @returns { HTMLElement } the card
  */
-function Card({image, value, type}) {
+function Card({ image, value, type }) {
   return (
-      <CardLabel>
-        <div>
-          <img src={image}>
-          </img>
+    <CardLabel>
+      <div>
+        <img src={image}></img>
+      </div>
+      <div>
+        <div className="card__value">
+          {" "}
+          {value} <span>{type === "Calories" ? "kCal" : "g"}</span>
         </div>
-        <div>
-          <div className="card__value"> {value} <span>{type === 'Calories' ? 'kCal' : 'g'}</span></div>
-          <div className="card__type"> {type} </div>
-        </div>
-      </CardLabel>
-  )
+        <div className="card__type"> {type} </div>
+      </div>
+    </CardLabel>
+  );
 }
 
 // Proptypes https://fr.reactjs.org/docs/typechecking-with-proptypes.html#proptypes
@@ -46,12 +48,13 @@ Card.propTypes = {
   image: PropTypes.string,
   value: PropTypes.number,
   type: PropTypes.string,
-}
+};
 
 Card.defaultProps = {
-  image: 'https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg',
+  image:
+    "https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg",
   value: 999,
-  type: 'Macro-nutriment',
-}
+  type: "Macro-nutriment",
+};
 
-export default Card
+export default Card;
