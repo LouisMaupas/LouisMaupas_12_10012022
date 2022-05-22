aa825ffcd6c953942a12979bc974471a7c418a68
lmaupas <louis.maupas@framatome.com>
2022-05-22 20:17:43 +0200

    fix(charts): UI

---
+++ sportsee/src/components/AllCards/AllCards.jsx
@@ -0,0 +1,45 @@
+// TODO si on passe par l'URL chart/... alors on flex row ?
+import Card from "../Cards/index";
+import styled from "styled-components";
+
+function AllCards({
+  iconCalories,
+  iconCarbs,
+  iconFat,
+  iconProtein,
+  userAccount,
+}) {
+  const CardsContainer = styled.div`
+    max-width: 250px;
+    max-height: 700px;
+  `;
+
+  return (
+    <CardsContainer>
+      <Card
+        image={iconCalories}
+        value={userAccount?.keyData ? userAccount.keyData.calorieCount : null}
+        type={"Calories"}
+      ></Card>
+      <Card
+        image={iconProtein}
+        value={
+          userAccount?.keyData ? userAccount.keyData.carbohydrateCount : null
+        }
+        type={"Proteines"}
+      ></Card>
+      <Card
+        image={iconCarbs}
+        value={userAccount?.keyData ? userAccount.keyData.lipidCount : null}
+        type={"Glucides"}
+      ></Card>
+      <Card
+        image={iconFat}
+        value={userAccount?.keyData ? userAccount.keyData.proteinCount : null}
+        type={"Lipides"}
+      ></Card>
+    </CardsContainer>
+  );
+}
+
+export default AllCards;
